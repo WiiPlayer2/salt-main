b7076094d71dee0a41a0cbdcfc2ffe3efc82f089
WiiPlayer2 <waldetomme@gmail.com>
2018-07-04 16:53:06 +0200

    Returning grains.set return value for debug purposes

--- _modules/counter.py
+++ _modules/counter.py
@@ -10,8 +10,8 @@ def init(id):
         if _name not in __grains__:
             __grains__[_name] = {}
         __grains__[_name][id] = 0
-        __salt__['grains.set']('{}:{}'.format(_name, id), 0)
-    return 0
+    return __salt__['grains.set']('{}:{}'.format(_name, id), 0)
+#    return 0
 
 def get(id):
     if exists(id):
@@ -29,8 +29,8 @@ def set(id, val):
     if not exists(id):
         init(id)
     __grains__[_name][id] = val
-    __salt__['grains.set']('{}:{}'.format(_name, id), val)
-    return val
+    return __salt__['grains.set']('{}:{}'.format(_name, id), val)
+#    return val
 
 #TODO
 #def remove(id):
